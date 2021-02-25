53ed612d1c9553558f112c0c91dd13f730965cd6
Szeti, balazs <bszeti@mtahq.org>
2021-02-25 13:48:46 -0500

    add jar manifest check

--- mta-generate-reports.sh
+++ mta-generate-reports.sh
@@ -68,7 +68,7 @@ do
     # Is it an executable JAR file?
     if [[ "$ARTIFACT" == *jar ]]
     then
-        unzip -p $ARTIFACT_WORK_DIR/*.jar META-INF/MANIFEST.MF | grep 'Main-Class'
+        unzip -p $ARTIFACT_WORK_DIR/*.jar META-INF/MANIFEST.MF | grep 'Main-Class' | tee -a "$MTA_RUN_LOG_FILE"
         if [ $? != 0 ]; then
             echo "Non-executable JAR file: $ARTIFACT" | tee -a "$MTA_RUN_LOG_FILE"
             rm -rf "$ARTIFACT_BASE_DIR"
