26f0c06d9de4030a86c4b5d52c99b51d417e5ef7
Sarah Zhong <szhong@redhat.com>
2021-04-26 18:51:22 -0400

    check for diff subdirs

--- source-mta-generate-reports.sh
+++ source-mta-generate-reports.sh
@@ -32,17 +32,17 @@ fi
 cat "$MTA_REPO_LIST_FILE" | while IFS="," read -r COL1 COL2 COL3 COL4 REPO BRANCH SUBDIR
 do
     # Download url
-    echo "Repo: '$REPO'" | tee -a "$MTA_RUN_LOG_FILE"
+    echo "Repo: '$REPO $SUBDIR'" | tee -a "$MTA_RUN_LOG_FILE"
 
     # Skip if repo is already in the .done file
-    grep -x "$REPO" "$MTA_REPO_DONE_FILE" >/dev/null && (echo "Skip $REPO" | tee -a "$MTA_RUN_LOG_FILE") && continue
+    grep -x "$REPO $SUBDIR" "$MTA_REPO_DONE_FILE" >/dev/null && (echo "Skip $REPO" | tee -a "$MTA_RUN_LOG_FILE") && continue
 
     # Trim
     REPO_NAME_RAW=$(basename "$REPO")
     REPO_NAME=$(echo "$REPO_NAME_RAW" | cut -f 1 -d '.')
     echo "Generate report for $REPO -> $REPO_NAME"
 
-    REPO_BASE_DIR="$MTA_REPORTS_OUTPUT_DIR/$REPO_NAME" 
+    REPO_BASE_DIR="$MTA_REPORTS_OUTPUT_DIR/$REPO_NAME-$SUBDIR" 
     REPO_REPORT_DIR="$REPO_BASE_DIR/report"
     REPO_WORK_DIR="$REPO_BASE_DIR/workdir"
     REPO_LOG_FILE="$REPO_WORK_DIR/mta-cli.log"
@@ -86,8 +86,8 @@ do
     echo "$REPO,$REPO_NAME,$POINTS,$NUM_TOTAL,$NUM_OPTIONAL,$NUM_CLOUD_MANDATORY,$NUM_CLOUD_OPTIONAL,$NUM_INFORMATION" | tee -a "$MTA_POINTS_FILE"
 
     # Mark as done
-    echo "$REPO" >>"$MTA_REPO_DONE_FILE"
-    echo "Done: '$REPO'" | tee -a "$MTA_RUN_LOG_FILE"
+    echo "$REPO $SUBDIR" >>"$MTA_REPO_DONE_FILE"
+    echo "Done: '$REPO $SUBDIR'" | tee -a "$MTA_RUN_LOG_FILE"
 
 
 done
